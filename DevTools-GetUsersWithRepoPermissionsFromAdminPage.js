79eb0e0ffd7e9165b2ee6918e17dedde4816f2fb
Adam Patridge <patridge@gmail.com>
2020-05-12 14:29:42 -0600

    Clean up code for easier understanding

--- DevTools-GetUsersWithRepoPermissionsFromAdminPage.js
+++ DevTools-GetUsersWithRepoPermissionsFromAdminPage.js
@@ -1,1 +1,8 @@
-[...($("#repository-access-table").getElementsByClassName("js-repo-access-entry"))].map(entry => `${entry.getElementsByTagName("strong")[0].textContent} (${entry.getElementsByTagName("a")[0].href}) - ${entry.getElementsByClassName("js-select-repo-permission")[0].getElementsByTagName("summary")[0].getElementsByTagName("span")[1].textContent.trim()}`).join("\n")
+[...($("#repository-access-table").getElementsByClassName("js-repo-access-entry"))]
+    .map(entry => {
+        let name = entry.getElementsByTagName("strong")[0].textContent;
+        let email = entry.getElementsByTagName("a")[0].href;
+        let access = entry.getElementsByClassName("js-select-repo-permission")[0].getElementsByTagName("summary")[0].getElementsByTagName("span")[1].textContent.trim();
+        return `${name} (${email}) - ${access}`;
+    })
+    .join("\n")
